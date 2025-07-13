b8c94c6da496dfb2691db69d40013cf55c9117b4
Joselon <jose.navarro.osta@gmail.com>
2025-07-13 23:29:08 +0000

    Add rooms controller and devise views

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,30 @@
+class RoomsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @rooms = Room.all
+  end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to @room
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_13_193533) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_13_222120) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -60,6 +60,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_13_193533) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "room_players", "rooms"
