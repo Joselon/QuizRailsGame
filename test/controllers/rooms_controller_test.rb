ec044939b0144beadaa751ab94197b00728fe95d
Joselon <jose.navarro.osta@gmail.com>
2025-07-15 10:02:19 +0000

    Fix tests fixtures

--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -1,23 +1,36 @@
 require "test_helper"
 
 class RoomsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    sign_in @user
+    @room = rooms (:one)
+  end
+
   test "should get index" do
-    get rooms_index_url
+    get rooms_url
     assert_response :success
   end
 
   test "should get show" do
-    get rooms_show_url
+    get room_url(@room)
     assert_response :success
   end
 
   test "should get new" do
-    get rooms_new_url
+    get new_room_url
     assert_response :success
   end
 
-  test "should get create" do
-    get rooms_create_url
-    assert_response :success
-  end
+#  test "should create room" do
+#    assert_difference("Room.count") do
+#      post rooms_url, params: {
+#        room: {name: "Sala de prueba", status: "waiting", current_turn: 0 }
+#      }
+#    end
+
+#    assert_redirected_to_room_url(Room.last)
+#  end
 end
