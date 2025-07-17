0cd3b5d27cdd859b4fe29a328f263d5000e93687
Joselon <jose.navarro.osta@gmail.com>
2025-07-17 00:23:58 +0000

    Fix enum rails 8 sintax

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,6 @@
 class Room < ApplicationRecord
+  enum :status, { waiting: 0, playing: 1, finished: 2 }
   has_many :room_players
   has_many :users, through: :room_players
   has_many :turns
-
-  enum status: { waiting: 0, playing: 1, finished: 2 }
 end
